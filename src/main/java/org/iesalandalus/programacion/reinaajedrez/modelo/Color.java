a3200999ec540c6ef4a80323bbdf7e8a81a7163f
Burhan <Burhan@DESKTOP-7QA8HJP>
2022-11-14 12:25:49 +0100

    He creado el enumerador Color, con los literales BLANCO y NEGRO, el atributo cadenaAMostrar, el constructor y el metodo toString

---
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
@@ -0,0 +1,23 @@
+package org.iesalandalus.programacion.reinaajedrez.modelo;
+
+public enum Color {
+	
+	
+	BLANCO("Blanco"),
+	NEGRO("Negro");
+	
+	
+	
+	String cadenaAMostrar;
+	
+	private Color(String cadena) {
+		this.cadenaAMostrar = cadena;
+	}
+	
+	public String toString() {
+		return this.cadenaAMostrar;
+	}
+	
+	
+	
+}
